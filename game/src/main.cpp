29a957314732cdf003debfb21ed73b28f2a41f83
phillipmont <phillip.monteiro5@gmail.com>
2023-05-17 17:35:43 -0400

    Lab 1 assignment complete.

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -5,9 +5,9 @@
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
-    SetWindowTitle("Space Defender 3000");
+    SetWindowTitle("Space Defender 3000"); //Set custom window title. Used for customizing the game window.
     Image icon = LoadImageFromTexture(LoadTexture("../game/assets/textures/1B.png"));
-    SetWindowIcon(icon);
+    SetWindowIcon(icon); //Set custom window icon. Used for customizing the game window.
     SetTargetFPS(60);
 
     //Background
@@ -29,16 +29,16 @@ int main(void)
 
 
     //Load Textures
-    Texture2D backgroundTex = LoadTexture("../game/assets/textures/bg_space.png");
+    Texture2D backgroundTex = LoadTexture("../game/assets/textures/bg_space.png"); //Load texture from file. Used to load textures into memory before drawing.
     Texture2D playerShipTex = LoadTexture("../game/assets/textures/1B.png");
     Texture2D enemyShip1Tex = LoadTexture("../game/assets/textures/8.png");
 
 
     //Load Audio
-    InitAudioDevice(); //Initialize audio device to play music and sounds
-    Music music = LoadMusicStream("../game/assets/audio/Cyberpunk Moonlight Sonata.mp3"); //Load music from file
-    PlayMusicStream(music); //Start playing loaded music
-    Sound laser = LoadSound("../game/assets/audio/laser.mp3"); //Load sound effect from file
+    InitAudioDevice(); //Initialize audio device. Needed to play music and sounds.
+    Music music = LoadMusicStream("../game/assets/audio/Cyberpunk Moonlight Sonata.mp3"); //Function #4: Load music from file. Must be loaded into memory to play.
+    PlayMusicStream(music); //Start playing loaded music. 
+    Sound laser = LoadSound("../game/assets/audio/laser.mp3"); //Load sound effect from file. Used to load sounds into memory.
 
 
     SetTargetFPS(60);
@@ -50,18 +50,18 @@ int main(void)
         if (IsKeyDown(KEY_Q))
             playerRotation -= playerRotationSpeed * dt;
         if (IsKeyDown(KEY_SPACE))
-            PlaySound(laser); //Play the "laser" sound effect when spacebar is pressed
+            PlaySound(laser); //Play the "laser" sound effect when spacebar is pressed.
 
-        UpdateMusicStream(music); //Updates buffers for music streaming
+        UpdateMusicStream(music); //Updates buffers for music streaming. Needed for continuous music playback.
 
-        const Vector2 mousePosition = GetMousePosition(); //Get mouse position coordinates (x,y)
+        const Vector2 mousePosition = GetMousePosition(); //Get mouse position coordinates (x,y). Needed for mouse inputs.
         const Vector2 playerSpritePosition{ mousePosition.x - (playerSize.x * 0.5f), mousePosition.y - (playerSize.y * 0.5f) };
         const Rectangle playerRec{ mousePosition.x, mousePosition.y, playerSize.x, playerSize.y };
         const Rectangle enemy1Rec{ 1000, 360, 182, 232 };
 
         //Collison Detection
         bool isColliding = false;
-        isColliding = CheckCollisionCircles(mousePosition, 70, enemyPosition, 120);
+        isColliding = CheckCollisionCircles(mousePosition, 70, enemyPosition, 120); //Checks for collison between 2 circles. Useful interaction between game objects.
         
         if (isColliding)
             enemyColor = { 255, 255, 0, 255 };
@@ -72,10 +72,10 @@ int main(void)
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawTexturePro(backgroundTex, BgSize, BgRec, { BgSize.x * 0.5f, BgSize.y * 0.5f }, 0.0f, WHITE);
+        DrawTexturePro(backgroundTex, BgSize, BgRec, { BgSize.x * 0.5f, BgSize.y * 0.5f }, 0.0f, WHITE); //Draw a loaded texture on the screen. Used to draw game assets.
 
         //Draw player
-        DrawCircleV(mousePosition, 70, BLUE);
+        DrawCircleV(mousePosition, 70, BLUE); //Draw a circle on the screen.
         DrawTexturePro(playerShipTex, playerShipRec, playerRec, { playerSize.x * 0.5f, playerSize.y * 0.5f }, playerRotation, WHITE);
 
         //Draw enemies
