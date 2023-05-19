8956738f8a34fcbf14da9d848373bd193a3985a5
phillipmont <phillip.monteiro5@gmail.com>
2023-05-19 16:58:57 -0400

    completed step 5

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -10,8 +10,11 @@ int main(void)
     rlImGuiSetup(true);
 
     Vector2 position = { 100,100 };
+    Vector2 position2 = { 100, 100 };
     Vector2 velocity = { 10, 0 };
-    float maxSpeed = 1000;
+    Vector2 acceleration = { 0, 50 };
+    float maxSpeed = 1000.0f;
+    float maxAcceleration = 1000.0f;
 
     SetTargetFPS(165);
 
@@ -28,8 +31,14 @@ int main(void)
         ImGui::SliderFloat2("velocity", &(velocity.x), -maxSpeed, maxSpeed);
         rlImGuiEnd();
 
-        Vector2 displacement = velocity * dt;
+        Vector2 deltaV = acceleration * dt;
+        velocity = velocity + deltaV; //px/s/s * s = px/s
+        Vector2 displacement = velocity * dt; //px/s * s = px
         position = position + displacement;
+
+        DrawLine()
+        
+        //position = WraparoundScreen(position);
 
         DrawCircleV(position, 50, GREEN);
         DrawFPS(10, 10);
